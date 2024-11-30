f6e7404e9f56d2b4bb7b1c372f41084afdabc527
delci <1211739@isep.ipp.pt>
2024-11-30 18:14:46 +0000

    1st commit

---
+++ src/main/java/rest/restapi/MicroServiceCalculatorApplication.java
@@ -0,0 +1,13 @@
+package rest.restapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroServiceCalculatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroServiceCalculatorApplication.class, args);
+    }
+
+}
